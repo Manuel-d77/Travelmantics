2acb7a4a47ee61feaf627cce0032f54f84b86a29
manuel-d77 <mydarko77@gmail.com>
2019-08-06 15:52:56 +0000

    Initial commit

---
+++ app/src/main/java/com/example/manuel/travelmantics/DealActivity.java
@@ -0,0 +1,200 @@
+package com.example.manuel.travelmantics;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DealActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private EditText title_editText,price_editText,description_editText;
+    private static final int PICTURE_RESULT=42;
+    ImageView imageView;
+    TravelDeal deal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deal);
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference =FirebaseUtil.mDatabaseReference;
+        title_editText= (EditText) findViewById(R.id.title_editText);
+        price_editText=(EditText)findViewById(R.id.price_editText);
+        description_editText=(EditText)findViewById(R.id.description_editText);
+        imageView=(ImageView)findViewById(R.id.image);
+
+        final Intent intent= getIntent();
+        TravelDeal deal=(TravelDeal) intent.getSerializableExtra("Deal");
+        if(deal==null){
+             deal= new TravelDeal();
+        }
+        this.deal=deal;
+        title_editText.setText(deal.getTitle());
+        description_editText.setText(deal.getDescription());
+        price_editText.setText(deal.getPrice());
+        showImage(deal.getImageUrl());
+        Button btnImage=(Button) findViewById(R.id.btnImage);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loadImage= new Intent(Intent.ACTION_GET_CONTENT);
+                loadImage.setType("image/jpeg");
+                loadImage.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(loadImage.createChooser(loadImage,"Insert Picture"),PICTURE_RESULT);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.save_menu,menu);
+        if (FirebaseUtil.isAdmin) {
+            menu.findItem(R.id.delete_menu).setVisible(true);
+            menu.findItem(R.id.save_menu).setVisible(true);
+            enableEditTexts(true);
+            findViewById(R.id.btnImage).setEnabled(true);
+        } else {
+            menu.findItem(R.id.delete_menu).setVisible(false);
+            menu.findItem(R.id.save_menu).setVisible(false);
+            enableEditTexts(false);
+            findViewById(R.id.btnImage).setEnabled(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this,"Deal successfully saved",Toast.LENGTH_LONG).show();
+                clean();
+                backToList();
+                return true;
+
+            case R.id.delete_menu:
+                deleteDeal();
+                Toast.makeText(this,"Deal successfully deleted",Toast.LENGTH_SHORT).show();
+                backToList();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PICTURE_RESULT&& resultCode==RESULT_OK){
+            Uri imageUri= data.getData();
+            final StorageReference ref= FirebaseUtil.mStorageRef.child(imageUri.getLastPathSegment());
+            ref.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> task= taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                    task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url=uri.toString();
+                            deal.setImageUrl(url);Log.d("Url: ", url);
+                            showImage(url);
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+
+    private void saveDeal() {
+        deal.setTitle(title_editText.getText().toString());
+        deal.setDescription(description_editText.getText().toString());
+        deal.setPrice(price_editText.getText().toString());
+
+        if (deal.getId() == null) {
+            mDatabaseReference.push().setValue(deal);
+        } else {
+            mDatabaseReference.child(deal.getId()).setValue(deal);
+        }
+    }
+
+    public void backToList(){
+        Intent intent= new Intent(this, ListActivity.class);
+        startActivity(intent);
+    }
+
+    public void deleteDeal(){
+        if (deal== null) {
+            Toast.makeText(this, "Please save the deal before deleting", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mDatabaseReference.child(deal.getId()).removeValue();
+        Log.d("image name", deal.getImageName());
+        if(deal.getImageName() != null && !deal.getImageName().isEmpty()) {
+            StorageReference picRef = FirebaseUtil.mStorage.getReference().child(deal.getImageName());
+            picRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Log.d("Delete Image", "Image Successfully Deleted");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("Delete Image", e.getMessage());
+                }
+            });
+        }
+    }
+
+    private void clean(){
+        title_editText.setText("");
+        description_editText.setText("");
+        price_editText.setText("");
+        title_editText.requestFocus();
+    }
+
+    private void enableEditTexts(boolean isEnabled){
+        title_editText.setEnabled(isEnabled);
+        description_editText.setEnabled(isEnabled);
+        price_editText.setEnabled(isEnabled);
+    }
+
+    private void showImage(String url){
+        if(url!=null&& !url.isEmpty()){
+            int width= Resources.getSystem().getDisplayMetrics().widthPixels;
+            Picasso.with(this)
+                    .load(url)
+                    .resize(width,width/1*3)
+                    .centerInside()
+                    .into(imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/manuel/travelmantics/FirebaseUtil.java
@@ -0,0 +1,128 @@
+package com.example.manuel.travelmantics;
+
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class FirebaseUtil {
+    public static FirebaseDatabase mFirebaseDatabase;
+    public static DatabaseReference mDatabaseReference;
+    private static FirebaseUtil mFirebaseUtil;
+   // private static FirebaseAnalytics mFirebaseAnalytics;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseStorage mStorage;
+    public static StorageReference mStorageRef;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
+    public static ListActivity caller;
+    private static int RC_SIGN_IN=123;
+    public static ArrayList<TravelDeal> myDeals;
+    public static boolean isAdmin;
+
+
+    private FirebaseUtil(){}
+
+    public static void openFbReference(String ref,final ListActivity callerActivity){
+        if(mFirebaseUtil ==null){
+            mFirebaseUtil = new FirebaseUtil();
+            mFirebaseDatabase = FirebaseDatabase.getInstance();
+            //mFirebaseAnalytics = FirebaseAnalytics.getInstance();
+            mFirebaseAuth=FirebaseAuth.getInstance();
+            caller=callerActivity;
+            mAuthListener= new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    if (firebaseAuth.getCurrentUser() == null) {
+                        FirebaseUtil.signIn();
+
+                    } else {
+                        String userId= firebaseAuth.getUid();
+                        checkAdmin(userId);
+                    }
+                    Toast.makeText(callerActivity.getBaseContext(),"Welcome back!",Toast.LENGTH_LONG).show();
+                }
+            };
+            connectStorage();
+
+        }
+        myDeals= new ArrayList<TravelDeal>();
+        mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    public static void attachListener(){
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public static void detachListener(){
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
+
+    }
+    private static void signIn(){
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                //new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());//PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+// Create and launch sign-in intent
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
+                        .setLogo(R.drawable.travel)      // Set logo drawable
+                        .setTheme(R.style.Theme_AppCompat)      // Set theme
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+    private static void checkAdmin(String uid){
+        FirebaseUtil.isAdmin=false;
+        DatabaseReference ref= mFirebaseDatabase.getReference().child("administrators").child(uid);
+        ChildEventListener listener= new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                FirebaseUtil.isAdmin=true;
+               caller.showMenu();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        ref.addChildEventListener(listener);
+    }
+    public static void connectStorage(){
+        mStorage=FirebaseStorage.getInstance();
+        mStorageRef= mStorage.getReference().child("deals_pictures");
+    }
+}
